83c24b771b65f9e378263b27155953f36c0efbbc
Ian Chege <ianchege001@gmail.com>
2023-06-01 20:29:56 +0300

    Added the Hero image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero1.png'
 
 export default function Home() {
   return (
-    <div className={styles.cointainer}>Welcome back!</div>
+    <div className={styles.cointainer}>
+      <Image src={Hero} alt='' className={styles.img} />
+    </div>
   )
 }
